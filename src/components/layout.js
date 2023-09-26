f07b806800eacd6495adcdc04769498976dd287f
l.westhead <luke.westhead@canterbury.ac.uk>
2023-09-26 16:10:11 +0100

    react bootstrap added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import parse from "html-react-parser"
+//Bootstrap stuff
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 
 const Layout = ({ isHomePage, children }) => {
   const {
@@ -20,6 +25,8 @@ const Layout = ({ isHomePage, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isHomePage}>
+      <Container>
+        <Row>
       <header className="global-header">
         {isHomePage ? (
           <h1 className="main-heading">
@@ -31,7 +38,8 @@ const Layout = ({ isHomePage, children }) => {
           </Link>
         )}
       </header>
-
+      </Row>
+      </Container>
       <main>{children}</main>
 
       <footer>
